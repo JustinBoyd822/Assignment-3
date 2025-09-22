05260776d3d50e3d0994524cde8342062c86eef1
Justin Boyd <118621446+JustinBoyd822@users.noreply.github.com>
2025-09-21 23:19:33 -0400

    Implement Node class with initializer
    Added a Node class for linked list data structures with an initializer.

--- node.py
+++ node.py
@@ -1,3 +1,15 @@
-# Implement your Node class here
 class Node:
-    pass # delete this line
+    """
+    A Node class for use in linked list-based data structures.
+    Each node contains a value and a pointer to the next node.
+    """
+    
+    def __init__(self, value):
+        """
+        Initialize a new Node with the given value.
+        
+        Args:
+            value: The data to store in this node
+        """
+        self.value = value
+        self.next = None
